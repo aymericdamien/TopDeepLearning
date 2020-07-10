5c826a12b78e4dd38f73ea77aad169d3610b7a4d
aymericdamien <aymeric.damien@gmail.com>
2020-07-09 21:15:16 -0700

    update script

--- scripts/generate_stats.py
+++ scripts/generate_stats.py
@@ -4,11 +4,13 @@ import operator
 import requests
 import time
 
+SKIP_LIST = ["awesome", "notebook", "learn", "curated list"]
+
 def search(keywords, n_pages=10, sort='stars'):
     res = []
     for k in keywords:
         for i in range(1, n_pages+1):
-            time.sleep(10)
+            time.sleep(20)
             url = "https://github.com/search?o=desc&q=%s&p=%d&s=%s&type=Repositories" % (k, i, sort)
             r = requests.get(url)
             info = extract_search_info(r.content)
@@ -20,23 +22,33 @@ def extract_search_info(html):
     info = []
     html = BeautifulSoup(html, 'html.parser')
     for c in html.find_all('li', {"class": "repo-list-item"}):
+        url = None
         try:
             stars = str(c.find_all('a', {"class": "muted-link"})[-1].text.strip())
             stars_unparsed = stars
             if 'k' in stars:
                 stars = float(stars.replace('k', '')) * 1000
             stars = int(stars)
-            lang = c.find('span', {'itemprop': "programmingLanguage"})
-            lang = lang.text.strip() if lang else lang
-            info.append({
-                "url": "https://github.com" + c.find('a', {"class": "v-align-middle"}).attrs["href"],
-                "title": c.find('a', {"class": "v-align-middle"}).text.strip(),
-                "desc": c.find('p', {"class": "col-12"}).text.strip(),
+            lang = None
+            url = "https://github.com" + c.find('a', {"class": "v-align-middle"}).attrs["href"]
+            title = c.find('a', {"class": "v-align-middle"}).text.strip().split('/')[-1]
+            desc = c.find('p', {"class": "mb-1"}).text.strip()
+            skip = 0
+            for w in SKIP_LIST:
+                if w in desc:
+                    skip = 1
+                    break
+            if skip == 0:
+              info.append({
+                "url": url,
+                "title": title,
+                "desc": desc.strip(),
                 "stars_unparsed": stars_unparsed,
                 "stars": stars,
                 "lang": lang
-            })
+              })
         except Exception as e:
+            print url
             print e
     return info
 
@@ -45,12 +57,10 @@ def get_topic(keywords, n_pages=10):
     next_token = None
     for k in keywords:
         for i in range(1, n_pages+1):
-            time.sleep(10)
-            url = "https://github.com/topics/%s" % k
-            if next_token:
-                url += "?after=%s" % next_token
+            time.sleep(20)
+            url = "https://github.com/topics/%s?page=%i" % (k, i)
             r = requests.get(url)
-            info, next_token = extract_topic_info(r.content)
+            info = extract_topic_info(r.content)
             for r in info:
                 res.append(r)
     return res
@@ -58,32 +68,42 @@ def get_topic(keywords, n_pages=10):
 def extract_topic_info(html):
     info = []
     html = BeautifulSoup(html, 'html.parser')
-    next_token = html.find('input', {"name": "after"}).attrs["value"]
-    for c in html.find_all('article', {"class": "py-4"}):
+    for c in html.find_all('article', {"class": "my-4"}):
+        url = None
         try:
-            stars = str(c.find_all('a', {"class": "link-gray"})[-1].text.strip())
+            stars = str(c.find_all('a', {"class": "social-count"})[-1].text.strip())
             stars_unparsed = stars
             if 'k' in stars:
                 stars = float(stars.replace('k', '')) * 1000
             stars = int(stars)
-            lang = c.find('span', {'itemprop': "programmingLanguage"})
-            lang = lang.text.strip() if lang else lang
-            info.append({
-                "url": "https://github.com" + c.find('h3').find('a').attrs["href"],
-                "title": c.find('h3').find('a').text.strip().replace(" / ", "/"),
-                "desc": c.find('div', {"class": "mb-3"}).text.strip(),
+            lang = None
+            url = "https://github.com" + c.find('h1').find_all('a')[1].attrs["href"]
+            time.sleep(5)
+            r = requests.get(url)
+            desc = BeautifulSoup(r.content, 'html.parser').find('p', {'class': 'f4'}).text
+            skip = 0
+            for w in SKIP_LIST:
+                if w in desc:
+                    skip = 1
+                    break
+            if skip == 0:
+              info.append({
+                "url": url,
+                "title": c.find('h1').find_all('a')[1].text.strip().replace(" / ", "/"),
+                "desc": desc.strip(),
                 "stars_unparsed": stars_unparsed,
                 "stars": stars,
                 "lang": lang
-            })
+              })
         except Exception as e:
+            print url
             print e
-    return info, next_token
+    return info
 
 def parse_results(results):
     results = {v['url']:v for v in results}.values()
     results = sorted(results, key=lambda x: x['stars'], reverse=True)
-    return [r for r in results if r['stars'] >= 1000 and (r['lang'] and r['lang'] != "TeX")]
+    return [r for r in results if r['stars'] >= 1000]
 
 def build_table(results_list):
 
@@ -101,8 +121,8 @@ def build_table(results_list):
     html += '</table>'
     return html, md
 
-topics = get_topic(['tensorflow', 'deep-learning', 'pytorch', 'machine-learning'], n_pages=5)
-searches = search(['tensorflow', 'deep learning', 'pytorch', 'cntk', 'machine learning'], n_pages=5)
+topics = get_topic(['tensorflow', 'deep-learning', 'pytorch', 'machine-learning'], n_pages=15)
+searches = search(['tensorflow', 'deep learning', 'pytorch', 'cntk', 'machine learning'], n_pages=15)
 
 r = parse_results(topics + searches)
 
